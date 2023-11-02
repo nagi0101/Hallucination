2bdd951eaba493d14c866438a2ad6feddc5a83e1
nagi0101 <demon0315@naver.com>
2023-11-02 22:04:42 +0900

    Dynamic gravity slope controll ver.1

--- Source/Hallucination/DynamicGravityCharacterComponent.cpp
+++ Source/Hallucination/DynamicGravityCharacterComponent.cpp
@@ -18,22 +18,23 @@ ACharacter* UDynamicGravityCharacterComponent::GetOwnerCharacter() const
 	return Cast<ACharacter>(ownerActor);
 }
 
-void UDynamicGravityCharacterComponent::DynamicGravity_Implementation(FVector Direction, float DeltaTime)
-{
-	ApplyDynamicGravity(Direction, DeltaTime);
-
-	TObjectPtr<UCharacterMovementComponent> movement = GetOwnerCharacter()->GetCharacterMovement();
-
-	FHitResult hit = GetDownLayHit();
-	if (hit.bBlockingHit)
-	{
-		movement->MovementMode = EMovementMode::MOVE_Walking;
-	}
-	else
-	{
-		movement->MovementMode = EMovementMode::MOVE_Falling;
-	}
-}
+//void UDynamicGravityCharacterComponent::DynamicGravity_Implementation(FVector Direction, float DeltaTime)
+//{
+//	ApplyDynamicGravity(Direction, DeltaTime);
+//
+//	TObjectPtr<UCharacterMovementComponent> movement = GetOwnerCharacter()->GetCharacterMovement();
+//
+//	FHitResult hit = GetDownLayHit();
+//	//UE_LOG(LogTemp, Warning, TEXT("%d"), movement->IsFalling())
+//	if (hit.bBlockingHit)
+//	{
+//		movement->MovementMode = EMovementMode::MOVE_Walking;
+//	}
+//	else
+//	{
+//		movement->MovementMode = EMovementMode::MOVE_Falling;
+//	}
+//}
 
 void UDynamicGravityCharacterComponent::ApplyDynamicGravity(FVector Direction, float DeltaTime)
 {
@@ -52,17 +53,56 @@ void UDynamicGravityCharacterComponent::ApplyDynamicGravity(FVector Direction, f
 	{
 		movement->MovementMode = EMovementMode::MOVE_Falling;
 	}
+	
+	const UPrimitiveComponent* HitComponent = hit.Component.Get();
+	bool walkable = true;
+	FVector slopeDown;
+	float relativeSlopeAngle = 0.0f;
+	if (HitComponent)
+	{
+		float relativeSlopeAngleRad = FMath::Acos(hit.Normal.Dot(-GetGravityDirection()));
+		relativeSlopeAngle = relativeSlopeAngleRad / PI * 180.f;
+
+		walkable = movement->GetWalkableFloorAngle() >= relativeSlopeAngle;
+		slopeDown = hit.Normal.Cross(GetGravityDirection().Cross(hit.Normal));
+		slopeDown.Normalize();
+	}
+		// Can't walk on this surface if it is too steep.
+		if (!walkable)
+		{
+			UE_LOG(LogTemp, Warning, TEXT("CAN'T WALK"));
+			Velocity += slopeDown * GetGravitationalAcceleration();
+			FHitResult slopeDownHit;
+			float traceLength = 100.f;
+			FVector from = hit.Location;
+			FVector to = from + slopeDown * traceLength;
+			GetWorld()->LineTraceSingleByChannel(slopeDownHit, from, to, ECollisionChannel::ECC_Visibility);
+
+			float speed = movement->Velocity.Dot(slopeDown);
+			GetOwnerCharacter()->AddActorWorldOffset(-slopeDown * speed * DeltaTime, true);
+			//GetOwnerCharacter()->SetActorLocation(slopeDownHit.Location, true);
+		}
+
+		// inv slope
+		if (relativeSlopeAngle > KINDA_SMALL_NUMBER && movement->IsWalkable(hit) != walkable)
+		{
+			UE_LOG(LogTemp, Warning, TEXT("inv slope"));
+			if (relativeSlopeAngle > 10.f)
+			{
+				float speed = movement->Velocity.Dot(slopeDown);
+				GetOwnerCharacter()->AddActorWorldOffset(slopeDown * speed * DeltaTime, true);
+			}
+		}
 
 	if (movement->IsFalling())
 	{
 		Velocity += Direction * DeltaTime * GetGravitationalAcceleration();
-		UpdateComponentVelocity();
-
-		movement->Velocity += Velocity;
-		movement->UpdateComponentVelocity();
 	}
 	else
 	{
-		Velocity = FVector::Zero();
+		Velocity = Direction * GetGravitationalAcceleration();
 	}
+	UpdateComponentVelocity();
+	movement->Velocity += Velocity;
+	movement->UpdateComponentVelocity();
 }
